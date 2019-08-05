5f5ea188fc63103ef62331cbb40d3b04ab9b3d72
me-govind <me.govindsharma@gmail.com>
2019-08-06 00:42:24 +0530

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# python-bank-api-data
+# Fetch Bank Details using Django REST APIs
+
+### Requirements
+1. Django 2 with [DRF](https://www.django-rest-framework.org/)
+2. Heroku
+
+## Usage Case:
+
+### GET bank detail by IFSC code
+```bash
+curl -X GET -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY1NDYzNTE1LCJqdGkiOiJhZjg2Njg5YzdhOTI0MTUzYTk5ZTU3OGQ5NzkwODA5NiIsInVzZXJfaWQiOjF9.33pSEIAM4ry8zGQFvIpW7EBa7o8DUT3v_FY__jmMR8U" https://python-bank-data-api.herokuapp.com/api/v1/bank_detail/ZSBL0000341
+```
+
+### GET bank details by city and bank name
+```bash
+curl -X GET -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTY1NDYzNTE1LCJqdGkiOiJhZjg2Njg5YzdhOTI0MTUzYTk5ZTU3OGQ5NzkwODA5NiIsInVzZXJfaWQiOjF9.33pSEIAM4ry8zGQFvIpW7EBa7o8DUT3v_FY__jmMR8U" https://python-bank-data-api.herokuapp.com/api/v1/bank_branches?offset=0&limit=5&city=LONI&bank_name=ZILA%20SAHAKRI%20BANK%20LIMITED%20GHAZIABAD
+```
--- bank_data/bank/views.py
+++ bank_data/bank/views.py
@@ -10,23 +10,6 @@ from bank_data.bank.models import BankInformation
 from bank_data.bank.serializers import BankInformationSerializer
 
 
-@api_view()
-def get_bank_detail(request, ifsc_code):
-    try:
-        bank = BankInformation.objects.get(ifsc=ifsc_code)
-    except BankInformation.DoesNotExist:
-        return Response({
-            'status': 'error',
-            'message': 'Bank not found with this ifsc code'
-        }, status=status.HTTP_404_NOT_FOUND)
-
-    ser_res = BankInformationSerializer(bank)
-    return Response({
-        'status': 'success',
-        'results': ser_res.data
-    }, status=status.HTTP_200_OK)
-
-
 class GetBankBranches(ListAPIView):
 
     serializer_class = BankInformationSerializer
@@ -48,26 +31,18 @@ class GetBankBranches(ListAPIView):
         }, status=status.HTTP_200_OK)
 
 
-# To upload CSV File
-def upload_file(request):
-    file_path = os.path.join('/Users/govindsharma/Mine/indian_banks/bank_branches.csv')
+@api_view()
+def get_bank_detail(request, ifsc_code):
+    try:
+        bank = BankInformation.objects.get(ifsc=ifsc_code)
+    except BankInformation.DoesNotExist:
+        return Response({
+            'status': 'error',
+            'message': 'Bank not found with this ifsc code'
+        }, status=status.HTTP_404_NOT_FOUND)
 
-    file_data = pd.read_csv(file_path)
-    for idx, row in file_data.iterrows():
-        data = row.to_dict()
-        try:
-            BankInformation.objects.get(
-                ifsc=data.get(''),
-                bank_id=data.get(''),
-                bank_name=data.get(''),
-                branch=data.get(''),
-                address=data.get(''),
-                city=data.get(''),
-                district=data.get(''),
-                state=data.get(''))
-        except CareType.DoesNotExist:
-            CareType.objects.update_or_create(
-                name=data.get('name'), is_license=is_license,
-                created_by=request.user.id,
-                updated_by=request.user.id
-            )
+    ser_res = BankInformationSerializer(bank)
+    return Response({
+        'status': 'success',
+        'results': ser_res.data
+    }, status=status.HTTP_200_OK)
